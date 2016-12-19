ea94f08e43f046774c46905419baabf447ae36ea
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2016-12-19 22:00:38 +0300

    "Go to Type Declaration" is broken for stdlib types (KT-13001)
     #KT-13001Fixed

--- idea/tests/org/jetbrains/kotlin/idea/navigation/AbstractGotoActionTest.kt
+++ idea/tests/org/jetbrains/kotlin/idea/navigation/AbstractGotoActionTest.kt
@@ -18,8 +18,16 @@ package org.jetbrains.kotlin.idea.navigation
 
 import com.intellij.codeInsight.CodeInsightActionHandler
 import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
+import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.testFramework.LightProjectDescriptor
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
 import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.test.KotlinLightProjectDescriptor
+import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
+import org.jetbrains.kotlin.idea.test.ProjectDescriptorWithStdlibSources
 import org.jetbrains.kotlin.test.KotlinTestUtils
 import org.junit.Assert
 
@@ -31,12 +39,48 @@ abstract class AbstractGotoActionTest : LightCodeInsightFixtureTestCase() {
 
         myFixture.configureByText(KotlinFileType.INSTANCE, parts[0])
 
-        val gotoSuperAction = ActionManager.getInstance().getAction(actionName) as CodeInsightActionHandler
-        gotoSuperAction.invoke(project, myFixture.editor, myFixture.file)
+        val gotoAction = ActionManager.getInstance().getAction(actionName) as CodeInsightActionHandler
+        gotoAction.invoke(project, myFixture.editor, myFixture.file)
 
-        val text = myFixture.getDocument(myFixture.file).text
-        val afterText = StringBuilder(text).insert(editor.caretModel.offset, "<caret>").toString()
+        val fileEditorManager = FileEditorManager.getInstance(myFixture.project) as FileEditorManagerEx
+        val currentEditor = fileEditorManager.selectedTextEditor ?: editor
 
-        Assert.assertEquals(parts[1], afterText)
+        if (currentEditor == editor) {
+            val text = myFixture.getDocument(myFixture.file).text
+            val afterText = StringBuilder(text).insert(editor.caretModel.offset, "<caret>").toString()
+
+            Assert.assertEquals(parts[1], afterText)
+        }
+        else {
+            val fileOffset = currentEditor.caretModel.offset
+            val lineNumber = currentEditor.document.getLineNumber(fileOffset)
+            val lineStart = currentEditor.document.getLineStartOffset(lineNumber)
+            val lineEnd = currentEditor.document.getLineEndOffset(lineNumber)
+            val inLineOffset = fileOffset - lineStart
+
+            val line = currentEditor.document.getText(TextRange(lineStart, lineEnd))
+            val withCaret = with(StringBuilder()) {
+                append(line)
+                insert(inLineOffset, "<caret>")
+                toString()
+            }
+
+            Assert.assertEquals(parts[1], withCaret)
+        }
+    }
+
+    override fun getProjectDescriptor() = getProjectDescriptorFromTestName()
+
+    protected fun getProjectDescriptorFromTestName(): LightProjectDescriptor {
+        val testName = StringUtil.toLowerCase(getTestName(false))
+
+        if (testName.endsWith("runtime")) {
+            return KotlinWithJdkAndRuntimeLightProjectDescriptor.INSTANCE
+        }
+        else if (testName.endsWith("stdlib")) {
+            return ProjectDescriptorWithStdlibSources.INSTANCE
+        }
+
+        return KotlinLightProjectDescriptor.INSTANCE
     }
 }

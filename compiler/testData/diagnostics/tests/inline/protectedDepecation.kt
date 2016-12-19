45d70aa41f53cbbc7853fdb22156e94b7e2a9291
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2016-12-19 13:58:38 +0100

    Published-api checker refactoring, support java package-protected visibility

--- compiler/testData/diagnostics/tests/inline/protectedDepecation.kt
+++ compiler/testData/diagnostics/tests/inline/protectedDepecation.kt
@@ -1,5 +1,13 @@
 // !DIAGNOSTICS: -EXPOSED_PARAMETER_TYPE -NOTHING_TO_INLINE
 
+// FILE: JavaClass.java
+
+
+public abstract class JavaClass {
+    protected void bind() {}
+}
+
+// FILE: main.kt
 open class A {
     protected fun test() {}
 
@@ -28,11 +36,37 @@ open class A {
         zVar
         <!PROTECTED_CALL_FROM_PUBLIC_INLINE!>zVar<!> = "123"
     }
+
+    protected inline fun callFromProtected() {
+        test()
+        zVar
+        zVar = "123"
+    }
+
 }
 
 class B : A() {
     inline fun testB() {
         <!PROTECTED_CALL_FROM_PUBLIC_INLINE!>test<!>()
+    }
+}
+
+class C : JavaClass() {
+    inline fun call() {
+        <!PROTECTED_CALL_FROM_PUBLIC_INLINE!>bind<!>()
+    }
+
+    internal inline fun callFromInternal() {
+        bind()
+    }
+
+    protected inline fun callFromProtected() {
+        bind()
+    }
+
+    @PublishedApi
+    internal inline fun callFromPublished() {
+        <!PROTECTED_CALL_FROM_PUBLIC_INLINE!>bind<!>()
     }
 }
 
